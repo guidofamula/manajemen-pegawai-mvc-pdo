b43a1846b4d5bd0699b2eed71f074f9a97a999d6
Guido Famula <guidofamula29@gmail.com>
2022-11-27 05:12:46 +0700

    pre setup databases & config

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DEVELOPMENT_ENVIRONMENT', true);
+define('BASE_PATH', 'http://localhost:41062/www/framework');
+define('DEFAULT_CONTROLLER', 'pegawai');
+
+define('DB_DRIVER', 'mysql');
+define('DB_NAME', 'pegawai');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+?>
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php 
+	echo "<h1>Hallo bro</h1>";
+?>
